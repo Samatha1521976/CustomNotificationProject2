abdc79472bd643ece1d19032839afed71cd18237
samatha_ramakrishna@yahoo.com <samatha_ramakrishna@yahoo.com>
2024-01-26 17:02:53 +0530

    Add project files.

---
+++ CustomNotificationProject2/CustomNotification.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNotificationProject2
+{
+    internal class CustomNotification
+    {
+        // Delegate definition
+        public delegate void Notify();
+
+        // Event declaration
+        public event Notify CustomNotificationEvent;
+
+        // Property definition
+        public string Notification { get; private set; }
+
+        // Method to handle subscription
+        private void OnSubscribe()
+        {
+            Notification = "Subscribed to custom notification";
+        }
+
+        // Method to handle unsubscription
+        private void OnUnsubscribe()
+        {
+            Notification = "Unsubscribed to custom notification";
+        }
+
+        // Method to manage subscription based on notify parameter
+        public void CustomNotificationSubscription(bool notify)
+        {
+            // Delist the previous subscriptions
+            CustomNotificationEvent -= OnSubscribe;
+            CustomNotificationEvent -= OnUnsubscribe;
+
+            // Subscribe based on the notify parameter
+            if (notify)
+            {
+                CustomNotificationEvent += OnSubscribe;
+            }
+            else
+            {
+                CustomNotificationEvent += OnUnsubscribe;
+            }
+
+            // Invoke the event to trigger the subscription method
+            CustomNotificationEvent?.Invoke();
+        }
+    }
+}
---
+++ CustomNotificationProject2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CustomNotificationProject2;
+
+var customNotification = new CustomNotification();
+
+customNotification.CustomNotificationSubscription(true);
+Console.WriteLine(customNotification.Notification);
+
+customNotification.CustomNotificationSubscription(false);
+Console.WriteLine(customNotification.Notification);
